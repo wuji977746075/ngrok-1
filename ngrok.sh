ea120f807193de0d6aabaf8728c61e5b9cc88932
rainbow <977746075@qq.com>
2021-09-22 12:33:32 +0800

    Update ngrok.sh
    change go source

--- ngrok.sh
+++ ngrok.sh
@@ -55,12 +55,12 @@ install_go(){
 	if [ $(getconf WORD_BIT) = '32' ] && [ $(getconf LONG_BIT) = '64' ];then
 		# 判断文件是否已经存在
 		if [ ! -f $SELFPATH/go1.7.6.linux-amd64.tar.gz ];then
-			wget https://www.golangtc.com/static/go/1.7.6/go1.7.6.linux-amd64.tar.gz --no-check-certificate
+			wget https://golang.google.cn/dl/go1.7.6.linux-amd64.tar.gz --no-check-certificate
 		fi
 	    tar zxvf go1.7.6.linux-amd64.tar.gz
 	else
 		if [ ! -f $SELFPATH/go1.7.6.linux-386.tar.gz ];then
-			wget https://www.golangtc.com/static/go/1.7.6/go1.7.6.linux-386.tar.gz --no-check-certificate
+			wget https://golang.google.cn/dl/go1.7.6.linux-386.tar.gz --no-check-certificate
 		fi
 	    tar zxvf go1.7.6.linux-386.tar.gz
 	fi
